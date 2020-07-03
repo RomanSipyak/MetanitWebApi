287a46d2c4089b477faaeda1a35178b7ad8b78e7
romansypiak <romansipyak@gmail.com>
2020-07-03 21:57:36 +0300

    Введение в фильтры

---
+++ src/Filters/Filters/LastVisit.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Filters.Filters
+{
+    public class LastVisit : Attribute, IResourceFilter
+    {
+        public void OnResourceExecuted(ResourceExecutedContext context)
+        {
+            //Will be exception becouse we already started our responce
+            //context.HttpContext.Response.Cookies.Append("LastVisit", DateTime.UtcNow.ToString("dd/mm/yyyy  hh-mm-ss"));
+        }
+
+        public void OnResourceExecuting(ResourceExecutingContext context)
+        {
+            //Now all good
+            context.HttpContext.Response.Cookies.Append("LastVisit", DateTime.UtcNow.ToString("dd/mm/yyyy hh-mm-ss" + "sync"));
+        }
+    }
+}
---
+++ src/Filters/Filters/LastVisitFilterAsync.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Filters.Filters
+{
+    public class LastVisitFilterAsync : Attribute, IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            //If we have to realization default use async filter
+            context.HttpContext.Response.Cookies.Append("LastVisit", DateTime.UtcNow.ToString("dd/mm/yyyy hh-mm-ss"));
+            await next();
+        }
+    }
+}
